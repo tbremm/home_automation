ae2a66b2a5ec7407bf588268027c49367615a15e
tbremm <timothy.bremm@gmail.com>
2020-06-23 15:02:17 -0700

    Added functioning temp sensor with terminal output

---
+++ ds18b20_temp_sensor_scratch.cpp
@@ -0,0 +1,54 @@
+// Interfacing with the DS18B20 temperature sensor
+// Code inspired by https://www.raspberrypi.org/forums/viewtopic.php?t=70709
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define SIZE 1
+#define NUMELEM 74
+
+using namespace std;
+
+int main ()
+{
+	FILE *fp = NULL;
+	char buff[100];
+	char temp_raw[5];
+    float temp_c = 0;
+    float temp_f = 0;
+	memset(buff,0,sizeof(buff));
+    const char* file_sensor = "/sys/bus/w1/devices/28-3c01d6073581/w1_slave";
+
+    while(true) {
+        
+        fp = fopen(file_sensor,"r");
+
+        if (NULL == fp) {
+            printf("\nfopen Error!!!\n\n");
+            return 1;
+        }
+
+        if(SIZE*NUMELEM != fread(buff,SIZE,NUMELEM,fp)) {
+            printf("\nfread() failed\n\n");
+            return 1;
+        }
+
+        temp_raw[0] = buff[69];
+        temp_raw[1] = buff[70];
+        temp_raw[2] = buff[71];
+        temp_raw[3] = buff[72];
+        temp_raw[4] = buff[73];
+        temp_raw[5] = buff[74];
+        
+        temp_c = atoi(temp_raw) / 1000.0;
+        temp_f = (temp_c / 5.0) * 9 + 32;
+
+        printf("Temp:\t%2.3f C\t%2.3f F\n", temp_c, temp_f);
+        delay(500);
+
+    }
+	return 0;
+}
